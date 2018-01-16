512af820474142d39ecf7abc032e8815c8db21bf
Miia Mähönen <miiamah@ety-a2123b-16.edu.wks.metropolia.fi>
2018-01-16 11:09:17 +0200

    b-osa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,23 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { AsideComponent } from './aside/aside.component';
+import { ContainerComponent } from './container/container.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavigationComponent,
+    FooterComponent,
+    MainComponent,
+    AsideComponent,
+    ContainerComponent
   ],
   imports: [
     BrowserModule
